a4382b2f1fe1b381d23a2b3c4bf65df4b0c61a39
Susan Kim <suzenyada@gmail.com>
2021-12-13 17:13:48 +0900

    Day 20 - Showing alert messages

--- GuessTheFlag/Shared/ContentView.swift
+++ GuessTheFlag/Shared/ContentView.swift
@@ -8,38 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
+    
     var body: some View {
-        VStack(spacing: 10) {
-            Button("Button1") { }
-            .buttonStyle(.bordered)
-            Button("Button2", role: .destructive) { }
-            .buttonStyle(.bordered)
-            Button("Button3") { }
-            .buttonStyle(.borderedProminent)
-            .tint(.mint)
-            Button("Button4", role: .destructive) { }
-            .buttonStyle(.borderedProminent)
-            
-            Button {
-                print("Button was tapped!")
-            } label: {
-                Text("Tap me!")
-                    .padding()
-                    .foregroundColor(.white)
-                    .background(.red)
-            }
-            
-            Button {
-                print("Edit button was tapped")
-            } label: {
-                Image(systemName: "pencil")
-            }
-            
-            Button {
-                print("Edit button was tapped")
-            } label: {
-                Label("Edit", systemImage: "pencil")
-            }
+        Button("Show Alert") {
+            showingAlert = true
+        }
+        .alert("Important message", isPresented: $showingAlert) {
+            // will set showing alert to false when dismissed
+            Button("Delete", role: .destructive) { }
+            Button("Cancel", role: .cancel) { }
+        } message: {
+            Text("Please read this")
         }
     }
     
