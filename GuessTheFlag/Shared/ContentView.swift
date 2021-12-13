6cdce783f889d59adc83e6f520467cf56286d495
Susan Kim <suzenyada@gmail.com>
2021-12-13 17:05:54 +0900

    Day 20 - Buttons and images

--- GuessTheFlag/Shared/ContentView.swift
+++ GuessTheFlag/Shared/ContentView.swift
@@ -9,7 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center)
+        VStack(spacing: 10) {
+            Button("Button1") { }
+            .buttonStyle(.bordered)
+            Button("Button2", role: .destructive) { }
+            .buttonStyle(.bordered)
+            Button("Button3") { }
+            .buttonStyle(.borderedProminent)
+            .tint(.mint)
+            Button("Button4", role: .destructive) { }
+            .buttonStyle(.borderedProminent)
+            
+            Button {
+                print("Button was tapped!")
+            } label: {
+                Text("Tap me!")
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(.red)
+            }
+            
+            Button {
+                print("Edit button was tapped")
+            } label: {
+                Image(systemName: "pencil")
+            }
+            
+            Button {
+                print("Edit button was tapped")
+            } label: {
+                Label("Edit", systemImage: "pencil")
+            }
+        }
+    }
+    
+    func executeDelete() {
+        print("Now deleting")
+
     }
 }
 
