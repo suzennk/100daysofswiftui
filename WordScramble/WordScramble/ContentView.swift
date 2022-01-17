4ce6f5f6271e081ee268f4162c00e8a5b62ae19b
Susan Kim <suzenyada@gmail.com>
2022-01-17 11:26:03 +0900

    Day 29 - Working with Strings

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -8,19 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var result1: String = ""
+    @State var result2: String = ""
+    @State var result3: String = ""
+    @State var result4: String = ""
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-    
-    func loadFile() {
-        if let fileURL = Bundle.main.url(forResource: "some-file",
-                                         withExtension: "txt") {
-            if let fileContents = try? String(contentsOf: fileURL) {
-                // we loaded the file into a String!
-                print(fileContents)
+        List {
+            Section("Random letter from [a b c]") {
+                Text(result1)
+            }
+            
+            Section("Separate multiline String") {
+                Text(result2)
+            }
+            
+            Section("Spell check [swift]") {
+                Text(result3)
+            }
+            
+            Section("Spell check [swiflljjfie]") {
+                Text(result4)
             }
         }
+        .onAppear {
+            test()
+            test2()
+            test3()
+            test4()
+        }
+    }
+    
+    func test() {
+        let input = "a b c"
+        let letters = input.components(separatedBy: " ")
+        let letter = letters.randomElement()
+        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
+        result1 = trimmed ?? ""
+    }
+    
+    func test2() {
+        let input = """
+a
+b
+c
+"""
+        let letters = input.components(separatedBy: "\n")
+        result2 = letters.joined(separator: ", ")
+    }
+    
+    func test3() {
+        let word = "swift"
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(
+            in: word,
+            range: range,
+            startingAt: 0,
+            wrap: false,
+            language: "en")
+        let allGood = misspelledRange.location == NSNotFound
+        result3 = allGood ? "Good!" : "Error in \(misspelledRange.description)"
+    }
+    
+    func test4() {
+        let word = "swiflljjfie"
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        let allGood = misspelledRange.location == NSNotFound
+        result4 = allGood ? "Good!" : "Error in \(misspelledRange.description)"
     }
 }
 
