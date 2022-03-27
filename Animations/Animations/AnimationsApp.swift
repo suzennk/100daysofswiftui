b6fe684c3109e6a2079823130783640f1703451f
Susan Kim <suzenyada@gmail.com>
2022-03-27 23:17:10 +0900

    Day 32 - Creating implicit animations

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by AV SW MacBook on 2022/03/27.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ImplicitAnimation()
+        }
+    }
+}
